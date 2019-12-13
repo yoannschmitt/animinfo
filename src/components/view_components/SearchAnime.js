463f17ac6b93964c536379e88634178b84b81fb7
Yoann SCHMITT <yoann.schmitt@etu.unistra.fr>
2019-12-13 12:23:38 +0100

    Gestion du changement de valeur de l'input

--- src/components/view_components/SearchAnime.js
+++ src/components/view_components/SearchAnime.js
@@ -1,12 +1,18 @@
-import React from "react";
+import React, { useState } from "react";
 import { InputGroup, FormControl } from 'react-bootstrap';
 
 const SearchAnime = (props) => {
+  const [name, setName] = useState("");
+
+  const handleNameChange = (event) => {
+    setName(event.target.value);
+  }
+
   return (
     <div className="col-11 col-sm-8 col-md-7">
         <label htmlFor="search-anime-input">Rechercher un animé</label>
         <InputGroup className="mb-3">
-            <FormControl className="p-4 input" placeholder="Rechercher un animé" aria-label="Rechercher un animé" id="search-anime-input"/>
+            <FormControl className="p-4 input" placeholder="Rechercher un animé" aria-label="Rechercher un animé" id="search-anime-input" onChange={e => handleNameChange(e)}/>
         </InputGroup>
     </div>
   );
