2138339be3205fd6b4b53c159d49561611baf65e
Yoann SCHMITT <yoann.schmitt@etu.unistra.fr>
2019-12-13 12:23:13 +0100

    Ajout des pages actives dans la navbar

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,14 +1,17 @@
 import React from "react";
 import { Navbar, Nav } from 'react-bootstrap';
 
+import { Link } from "react-router-dom";
+
 const Header = (props) => {
   return (
     <Navbar bg="dark" variant="dark">
         <Navbar.Brand href="#home">Navbar</Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />  
         <Nav className="mr-auto">
-            <Nav.Link href="/">Accueil</Nav.Link>
-            <Nav.Link href="/top">Top 50</Nav.Link>
-            <Nav.Link href="#pricing">Favoris</Nav.Link>
+            <Nav.Link as={Link} to="/">Accueil</Nav.Link>
+            <Nav.Link as={Link} to="/top">Top 50</Nav.Link>
+            <Nav.Link as={Link} to="#pricing">Favoris</Nav.Link>
         </Nav>
     </Navbar>
   );
